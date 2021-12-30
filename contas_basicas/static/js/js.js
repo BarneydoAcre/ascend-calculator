d505b6cb21910b4759888b2c72c49c2e11509075
unknown <gabrielfauth@outlook.com>
2021-12-29 22:03:02 -0400

    calculadora finalizada

--- contas_basicas/static/js/js.js
+++ contas_basicas/static/js/js.js
@@ -1,2 +1,18 @@
-let teste = 'teste'
-console.log(teste)
+function insert(num){
+    var numero = document.getElementById('result').innerHTML
+    document.getElementById('result').innerHTML = numero + num
+}
+
+function clean(){
+    document.getElementById('result').innerHTML = ''
+}
+
+function back(){
+    var numero = document.getElementById('result').innerHTML
+    document.getElementById('result').innerHTML = numero.substring(0, numero.length -1)
+}
+
+function calculate(){
+    let numero = document.getElementById('result').innerHTML
+    document.getElementById('result').innerHTML = eval(numero)
+}
